9ba734c816d0d1ab48c0e8c7c972b120815b3075
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2022-10-30 14:25:22 +0000

    Even prettier

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -8,5 +8,7 @@ test("findShortestString finds the shortest string in an array", () => {
 
 test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
   expect(findShortestString(["brave", "dance"])).toBe("brave");
-  expect(findShortestString(['the','good','the','bad','and','the','ugly'])).toBe('the');
+  expect(
+    findShortestString(["the", "good", "the", "bad", "and", "the", "ugly"])
+  ).toBe("the");
 });
